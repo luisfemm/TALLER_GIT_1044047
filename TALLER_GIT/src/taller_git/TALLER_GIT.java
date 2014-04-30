1552b934e1ddfcb5d9ef4d0c51c0e262178760ba
luisfemm <luisfemm@outlook.com>
2014-04-30 17:37:53 -0500

    Proyecto Inicial

---
+++ TALLER_GIT/src/taller_git/TALLER_GIT.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller_git;
+
+/**
+ *
+ * @author pipe
+ */
+public class TALLER_GIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
