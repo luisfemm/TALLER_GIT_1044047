c70729c59fab6fce75653a5c62261ce53e9d4db3
sauletheb <sauletheb@gmail.com>
2014-04-30 23:01:29 -0500

    Subiendo  la clase AlmacenDeVenta.

---
+++ TALLER_GIT/src/taller_git/AlmacenDeVenta.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller_git;
+
+/**
+ *
+ * @author eallanosm
+ */
+public class AlmacenDeVenta {
+    
+    String codigo;
+    String nombre;
+    String direccion;
+    String numeroTelefonico;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNumeroTelefonico() {
+        return numeroTelefonico;
+    }
+
+    public void setNumeroTelefonico(String numeroTelefonico) {
+        this.numeroTelefonico = numeroTelefonico;
+    }
+    
+}
